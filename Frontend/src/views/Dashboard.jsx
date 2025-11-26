b8c5318db32b4c8c8ff8915309ebdcf5e0abd7ef
Hamburgir <shantnuprofid@gmail.com>
2025-11-27 00:46:30 +0530

    Added method for uploading file

--- Frontend/src/views/Dashboard.jsx
+++ Frontend/src/views/Dashboard.jsx
@@ -1,11 +1,24 @@
 import FileUploadButton from "../components/fileUploadButton";
+import { uploadFile } from "../utils/fileApi";
 
 
 function Dashboard() {
+  const [files, setFiles] = useState([]);
+
+  const handleUpload = async (file) => {
+    try {
+      const created = await uploadFile(token, file);
+      // TODO: if backend returns url, push to list
+      setFiles((s) => [created, ...s]);
+    } catch (err) {
+      alert("Upload failed: " + (err.message || err));
+    }
+  };
+
   return (
     <div className="Dashboard">
       {/* <button onClick={}> upload </button> */}
-      <FileUploadButton onUpload={(file) => console.log(file)} />
+      <FileUploadButton onUpload={handleUpload} />
     </div>
   )
 }
